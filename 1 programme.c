95e20ea9544c07c7a1afbd7707ce5f8e5ad25bb1
tanzeelurehman <56665955+tanzeelurehman@users.noreply.github.com>
2019-10-28 20:57:14 +0500

    Add files via upload

---
+++ 1 programme.c
@@ -0,0 +1,31 @@
+#iclude<stdio.h>
+
+int main(void
+
+int day;
+printf("choose a subject/n1.Fundamental programming/n3.Applied physics/n4.English/n.5Calculus/n6.canel/n);
+printf("nICT");)
+scanf ("%d", & day);
+if (day ==1);
+}
+printf("nICT");
+}
+else (if day==2)
+    }
+prinf("/n Fundamental programming/n;
+}
+else if(day==3)
+    prinf(/nApplied physics/n");
+}
+else(if day==4)
+    }
+    else if (day==5);
+    }
+printf(/nEnglish/n");
+}
+else il(/nCalculas/n);
+ }
+ else}
+ printf(/nchoose a correct option /n");
+ return 0;
+]
