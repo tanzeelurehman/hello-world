1b86807d407513c5bf371af35a57c3b2f659b5a1
tanzeelurehman <56665955+tanzeelurehman@users.noreply.github.com>
2019-10-28 21:05:59 +0500

    Add files via upload

---
+++ programme.c
@@ -0,0 +1,12 @@
+1 #include<stdio.h>
+2
+3main<>}
+4
+5
+6float meters, inches
+7printf(" Enter no of meters:/n")
+89scanf("%f" &meters);
+9meters=inches * 39.37;
+10printf("%f inches" ,inches);
+11return 0;
+12}
